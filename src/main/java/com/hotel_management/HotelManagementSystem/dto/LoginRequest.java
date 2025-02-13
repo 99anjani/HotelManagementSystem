81b119b3774ae1a68520d2cc6cb03a3b7c003ac4
Nethmi Anjani <anjaninethmi23@gmail.com>
2025-02-13 10:08:42 +0530

    DTO Updated

---
+++ src/main/java/com/hotel_management/HotelManagementSystem/dto/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.hotel_management.HotelManagementSystem.dto;
+
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @NotBlank(message = "Email is Required")
+    private String email;
+    @NotBlank(message = "Password is required")
+    private String password;
+}
